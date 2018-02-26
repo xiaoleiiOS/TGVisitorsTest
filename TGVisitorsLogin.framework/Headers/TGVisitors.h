92a4ad6513641da4379a31d74be70ed448a5754d
wangxiaolei <675407568@qq.com>
2018-02-26 12:04:08 +0800

    TG游客
    TG游客登录framework

---
+++ TGVisitorsLogin.framework/Headers/TGVisitors.h
@@ -0,0 +1,16 @@
+//
+//  TGVisitorsLogin.h
+//  TGameSDKTestDemo
+//
+//  Created by TW-C-060 on 2018/2/9.
+//  Copyright © 2018年 xiaolei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**游客登录*/
+//static NSString *const TGVisitorsLogin = @"tg://visitors/login";
+
+@interface TGVisitors : NSObject
+
+@end
